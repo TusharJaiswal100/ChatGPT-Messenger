566543403c7a34db3792b06e03731041ad1b5534
Tushar Jaiswal <73294551+TusharJaiswal100@users.noreply.github.com>
2023-03-22 17:57:36 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -2,7 +2,7 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyAJaJDShKfNaLBKt0KMdvtiOoiPhCzMuII",
   authDomain: "chatgpt-messanger-fdf56.firebaseapp.com",
@@ -12,7 +12,7 @@ const firebaseConfig = {
   appId: "1:1801322653:web:da2a844e8988bd485fd474"
 };
 
-// Initialize Firebase
+
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
