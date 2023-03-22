f47caf1e4ff70066867dc60d9878c4c4975e723e
Tushar Jaiswal <73294551+TusharJaiswal100@users.noreply.github.com>
2023-03-22 17:50:01 +0530

    Update Login.tsx

--- components/Login.tsx
+++ components/Login.tsx
@@ -6,14 +6,14 @@ import Image from "next/image";
   return (
     <div className="bg-[#11A37F] h-screen flex flex-col items-center 
     justify-center text-center">
-    <Image        // chatgpt logo
+    <Image       
         src="https://links.papareact.com/2i6"
         width={300}
         height={300}
         alt="logo"
     /> 
     <button onClick={() => signIn("google")} className="text-white font-bold text-3xl animate-pulse">Sign In to use ChatGPT</button> 
-    {/* by using above onclick line we are doing authetication with google video at 1:13:00 */}
+   
     </div> 
   )
 }
