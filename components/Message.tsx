c203551940f581957c18cdae6105e89508016313
Tushar Jaiswal <73294551+TusharJaiswal100@users.noreply.github.com>
2023-03-22 17:50:27 +0530

    Update Message.tsx

--- components/Message.tsx
+++ components/Message.tsx
@@ -5,14 +5,13 @@ type Props = { //typescript
 };
 
 function Message({message}: Props) {
-  const isChatGPT = message.user.name === "ChatGPT"; // to check if it is user or chatgpt
+  const isChatGPT = message.user.name === "ChatGPT"; 
 
   return (
-    <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}> {/* {isChatGPT && "bg-[#434654]"} this line means the area at which chatgpt written the answer make it color to #434654 color */}
+    <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}> 
         <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
-            <img src={message.user.avatar} alt="" className="h-8 w-8"/> {/* this line shows the profile photo of user , if user is human it will show their photo and if chatgpt is replyinh then it will show photo of chatgpt */}
-            <p className="pt-1 text-sm">
-                {message.text} {/* this line show the question asked by user and answer replied by chatgpt */}
+            <img src={message.user.avatar} alt="" className="h-8 w-8"/>
+                {message.text} 
             </p>
         </div>
     </div>
