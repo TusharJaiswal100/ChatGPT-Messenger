8108527251aa3dd4b7cdbec8f06b25c8d23abf32
Tushar Jaiswal <73294551+TusharJaiswal100@users.noreply.github.com>
2023-03-22 17:52:25 +0530

    Update chatgpt.ts

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY, // it acts as a key to access openai api
+    apiKey: process.env.OPENAI_API_KEY, 
 })
 
 const openai= new OpenAIApi(configuration)
