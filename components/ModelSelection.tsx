9976ca6a8041148a70394d5343438b6f2aaa2e08
Tushar Jaiswal <73294551+TusharJaiswal100@users.noreply.github.com>
2023-03-22 17:51:03 +0530

    Update ModelSelection.tsx

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -3,12 +3,12 @@
 import useSWR from "swr";
 import Select from "react-select";
 
-const fetchModels = () => fetch("/api/getEngines").then((res) => res.json()); // calling the api // passing it to json
+const fetchModels = () => fetch("/api/getEngines").then((res) => res.json()); 
 
 function ModelSelection() {
-  const {data: models, isLoading} = useSWR("models", fetchModels); // here in place of "models" is a key but we can write it anything 
-  // above line will fetch information
-  const { data: model, mutate: setModel  } = useSWR("model", { // here initially  "text-davinci-003" will be diplayed and when user select option then that model will be displayed
+  const {data: models, isLoading} = useSWR("models", fetchModels); 
+  
+  const { data: model, mutate: setModel  } = useSWR("model", { 
     fallbackData: "text-davinci-003",
   });
    
@@ -16,16 +16,16 @@ function ModelSelection() {
     <div className="mt-2">
         <Select
            className="mt-2"
-           options = {models?.modelOptions} // what options we have to show when selecting 
+           options = {models?.modelOptions} 
            defaultValue={model}
            placeholder={model}
-           isSearchable       // we can search by typing
+           isSearchable       
            isLoading = {isLoading}
            menuPosition = "fixed"
            classNames={{
              control: (state) => "bg-[#434654] border-[#434654]",
            }}
-           onChange={(e) => setModel(e.value)} // this will set the model on selecting
+           onChange={(e) => setModel(e.value)} 
         />
     </div>
   )
@@ -33,5 +33,3 @@ function ModelSelection() {
 
 export default ModelSelection
 
-// rather than peace of state which user select in the drop down list 
-// we can have another catched value using useSWR
